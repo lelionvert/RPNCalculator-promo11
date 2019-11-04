b4bc49b253f466c7b02f8de9cc34521e1b339fd0
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 17:53:34 +0100

    0 = 0

--- src/test/java/fr/coachs/CoachTest.java
+++ src/test/java/fr/coachs/CoachTest.java
@@ -1,10 +1,11 @@
 package fr.coachs;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CoachTest {
     @Test
     void addition_of_0_and_0_should_return_0() {
-
+        Assertions.assertThat(0).isEqualTo(0);
     }
 }
