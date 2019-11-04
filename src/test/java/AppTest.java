832e56885976d0ef2012786ccdc681c1dd5d5be5
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 14:44:17 +0100

    Parameterized Test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,21 +10,14 @@ public class AppTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"0 4 +, 4", "0 3 +, 3", "0 5 +, 5"})
+    @CsvSource(value = {
+            "0 4 +, 4",
+            "0 3 +, 3",
+            "0 5 +, 5"
+    })
     public void addition_of_zero_and_n_number_returns_n(String entry, int expected) {
         Assertions.assertThat(calculate(entry)).isEqualTo(expected);
     }
-
-    @Test
-    public void test3() {
-        Assertions.assertThat(calculate("0 3 +")).isEqualTo(3);
-    }
-
-    @Test
-    public void test4() {
-        Assertions.assertThat(calculate("0 5 +")).isEqualTo(5);
-    }
-
 
     private int calculate(String s) {
         return Integer.parseInt(s.split(" ")[1]);
