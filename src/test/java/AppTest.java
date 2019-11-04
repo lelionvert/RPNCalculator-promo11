2783ca70e158f9f5792d180770327a823f7c5f24
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 13:54:43 +0100

    First test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     @Test
     public void test() {
-        Assertions.assertThat(calculate()).isEqualTo(0);
+        Assertions.assertThat(calculate("0 0 +")).isEqualTo(0);
     }
 
-    private int calculate() {
+    private int calculate(String s) {
         return 0;
     }
 }
