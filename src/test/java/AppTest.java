2c115878fcab6ccb93d8b3ea37da45a851b0e0f0
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 13:45:10 +0100

    Environment ready

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void test() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
