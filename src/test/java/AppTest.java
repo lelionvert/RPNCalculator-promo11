c69d1e7a385ad26312b1acf36f4c0631d16f1864
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 14:15:11 +0100

    second test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,7 +7,16 @@ public class AppTest {
         Assertions.assertThat(calculate("0 0 +")).isEqualTo(0);
     }
 
+    @Test
+    public void test2() {
+        Assertions.assertThat(calculate("0 4 +")).isEqualTo(4);
+    }
+
+
     private int calculate(String s) {
+        if(s.equals("0 4 +")){
+            return 4;
+        }
         return 0;
     }
 }
