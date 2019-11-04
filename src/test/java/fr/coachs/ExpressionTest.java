7859ca10fd1a37fe15c5b3f80eaea1cd7bc49dd0
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 18:18:10 +0100

    Optimize imports

--- src/test/java/fr/coachs/ExpressionTest.java
+++ src/test/java/fr/coachs/ExpressionTest.java
@@ -1,10 +1,8 @@
 package fr.coachs;
 
-import fr.coachs.Expression;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ExpressionTest {
 
