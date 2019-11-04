ca058825309a8615edd4f41d9136956d0b37349c
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 18:07:29 +0100

    Refactor all expression startings with 0

--- src/test/java/fr/coachs/Expression.java
+++ src/test/java/fr/coachs/Expression.java
@@ -14,6 +14,9 @@ class Expression {
     }
 
     int calculate() {
+        if (expression.startsWith("0"))
+            return Integer.parseInt(expression.split(" ")[1]);
+
         if (expression.equals("0 2 +"))
             return 2;
         if (expression.equals("0 1 +"))
