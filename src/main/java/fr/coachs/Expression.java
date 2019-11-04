a844ef3df9df31dd91c11f7886c002c736544423
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-04 18:32:25 +0100

    Remove duplication

--- src/main/java/fr/coachs/Expression.java
+++ src/main/java/fr/coachs/Expression.java
@@ -15,10 +15,6 @@ class Expression {
 
     Result calculate() {
         String[] terms = expression.split(" ");
-
-        if (terms[0].equals("0"))
-            return Result.of(Integer.parseInt(terms[1]));
-
         return Result.of(Integer.parseInt(terms[0]) + Integer.parseInt(terms[1]));
     }
 }
