c1eefd049633430486c636a7fe73069c39fe08c6
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-06 11:48:20 +0100

    add bifunction in Operator

--- src/main/java/fr/lacombe/Operator.java
+++ src/main/java/fr/lacombe/Operator.java
@@ -2,19 +2,22 @@ package fr.lacombe;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public enum Operator {
-    MULTIPLICATION("*", Operation::operate),
-    ADDITION("+", Operation::operate);
+    MULTIPLICATION("*", Operation::operate, Multiplication::new),
+    ADDITION("+", Operation::operate, Addition::new);
 
     private static final List<Operator> values = List.of(values());
     public final String operator;
     public final Function<Operation, Integer> operation;
+    public final BiFunction<Integer, Integer, Operation> operation2;
 
-    Operator(String operator, Function<Operation, Integer> operation) {
+    Operator(String operator, Function<Operation, Integer> operation, BiFunction<Integer, Integer, Operation> operation2) {
         this.operator = operator;
         this.operation = operation;
+        this.operation2 = operation2;
     }
 
     static boolean isOperator(String element) {
