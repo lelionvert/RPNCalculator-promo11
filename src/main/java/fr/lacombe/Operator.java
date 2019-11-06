9d91102403d3198dde87bce7fa10c9071d7d0e7e
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-06 10:06:18 +0100

    Refactor operator

--- src/main/java/fr/lacombe/Operator.java
+++ src/main/java/fr/lacombe/Operator.java
@@ -1,17 +1,20 @@
 package fr.lacombe;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 
 public enum Operator {
-    MULTIPLICATION("*"),
-    ADDITION("+");
+    MULTIPLICATION("*", (a, b) -> a * b),
+    ADDITION("+", (a, b) -> a + b);
 
     private static final List<Operator> values = List.of(values());
     public final String operator;
+    private final BinaryOperator<Integer> operation;
 
-    Operator(String operator) {
+    Operator(String operator, BinaryOperator<Integer> operation) {
         this.operator = operator;
+        this.operation = operation;
     }
 
     static Predicate<String> isOperator() {
